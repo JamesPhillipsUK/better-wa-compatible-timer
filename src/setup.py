ced8d896a5a0e6e26efc977052c087a4d745a007
Jesse Phillips <JamesPhillipsUK@users.noreply.github.com>
2026-05-09 00:12:26 +0100

    Initial code file setup.

---
+++ src/setup.py
@@ -0,0 +1,5 @@
+""" setup.py
+
+Author: Jesse Phillips <jesse@jessephillips.uk>
+Version 0.0.1
+"""
---
+++ src/timer.py
@@ -0,0 +1,6 @@
+""" setup.py
+
+Author: Jesse Phillips <jesse@jessephillips.uk>
+Version 0.0.1
+"""
+
